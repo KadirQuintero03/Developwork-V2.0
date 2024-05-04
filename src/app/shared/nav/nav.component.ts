c38f481581effdc4311096e2dc8fc02fe9280424
KadirQuintero03 <kadireq1928@gmail.com>
2024-05-04 14:40:34 -0500

    cambiando cositas

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -6,8 +6,8 @@ import { PersonaService } from '../../services/personas/persona.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css'],
 })
+
 export class NavComponent implements OnInit {
   constructor(
     private router: Router,
